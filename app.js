46fd2872e2a10a027670f2aed60409f44c5c157e
Daniel Samson Raj <danielsam458@gmail.com>
2020-01-25 05:40:34 -0800

    Update app.js

--- app.js
+++ app.js
@@ -18,28 +18,33 @@ function runApp(userChoice) {
             document.getElementById('p1').innerHTML='Thats OUT !! Now its time to bowl and restrict a target of ' + userScore ;
             canUserPlay=false;
             canComputerPlay=true;
+            document.getElementById('t11').innerHTML="";
         }
         else {
             userScore = eval(userScore) + eval(userChoice);
             document.getElementById('p1').innerHTML="Thats a " + dialogues[userChoice-1] + "....";
-            document.getElementById('p2').innerHTML = userScore;
-            document.getElementById('p3').innerHTML = computerScore;
+            document.getElementById('t11').innerHTML="*";
+            document.getElementById('t12').innerHTML = userScore;
+            document.getElementById('t14').innerHTML = computerScore;
         }
     }
     else if(canComputerPlay) {
         if(computerChoice==userChoice) {
             document.getElementById('p1').innerHTML="OUT !! You Won the match " ;
             canComputerPlay=false;
+            document.getElementById('t13').innerHTML="";
         }
         else{
             computerScore=eval(computerChoice) + eval(computerScore);
             document.getElementById('p1').innerHTML="Thats a " + dialogues[userChoice-1] + "....";
             if(computerScore > userScore) { 
                 document.getElementById('p1').innerHTML+=' Computer Won The match ';
+                document.getElementById('t11').innerHTML="";
                 canComputerPlay=false;
             }
-            document.getElementById('p2').innerHTML = userScore;
-            document.getElementById('p3').innerHTML = computerScore;
+            document.getElementById('t13').innerHTML="*";
+            document.getElementById('t12').innerHTML = userScore;
+            document.getElementById('t14').innerHTML = computerScore;
         }
     }
 }
